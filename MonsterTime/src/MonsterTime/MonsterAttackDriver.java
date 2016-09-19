46b7efe095455daf63738c9d062c34a0e2332ec2
Daniel Caceres <mattmoocow@live.com>
2016-09-19 11:43:29 -0700

    Delete MonsterAttackDriver.java

--- MonsterTime/src/MonsterTime/MonsterAttackDriver.java
+++
@@ -1,9 +0,0 @@
-package MonsterTime;
-
-public class MonsterAttackDriver {
-	public static void main(String[] args){
-			AttackMonitor getDestroyed = new AttackMonitor();
-			getDestroyed.monitor();
-	}
-	
-}
