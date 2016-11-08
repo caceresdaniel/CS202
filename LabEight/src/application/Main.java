367cf5d766734d199f3da38440f29416fcbc1bad
Daniel Caceres <mattmoocow@live.com>
2016-11-07 22:23:39 -0800

    Add files via upload

--- LabEight/src/application/Main.java
+++ LabEight/src/application/Main.java
@@ -1,8 +1,5 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.JOptionPane;
 
 import javafx.application.Application;
@@ -16,13 +13,13 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	public int row;
-	public int col;
-	public Label lbl = new Label();
+	Label[][] lbl;
+	GridPane gp = new GridPane();
+	Button butt = new Button("Find Problems");
+	TextField userInput = new TextField();
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -32,114 +29,29 @@ public class Main extends Application {
 			BorderPane bp = new BorderPane();
 			bp.getStyleClass().add("grid");
 
-			Scene sc = new Scene(bp, 835, 905);
+			Scene sc = new Scene(bp);
 			sc.getStylesheets().add("styles/style.css");
-			
-			GridPane gp = new GridPane();
 
 			Label title = new Label("Reverse Multiplication Table");
 			title.getStyleClass().add("mainTitle");
 
 			Label askAnswerLabel = new Label("Enter Answer: ");
-			askAnswerLabel.getStyleClass().add("answerLabel");
-
-			Button butt = new Button("Find Problems");
-
-			final TextField userInput = new TextField();
-			//userInput.getStyleClass().add("inputBar");
-			//userInput.setMaxWidth(100);
+			askAnswerLabel.getStyleClass().add("askAnswerLabel");
 
 			VBox vb = new VBox();
 
 			HBox hb = new HBox();
 			hb.getStyleClass().add("horizontal");
 
+			// call the grid making method here
+			setLabels();
 
-			for (row = 0; row < 11; row++)
-				for (col = 0; col < 11; col++) {
-
-					Rectangle rect = new Rectangle();
-					// Label lbl = new Label();
-
-					
-					//Creating the labels and adding the styles for boxes and text
-					if (row == 0 || col == 0) {
-						if (row == 0 && col != 0)
-							lbl = new Label("    " + String.valueOf(col));
-						else if (col == 0 && row != 0)
-							lbl = new Label("    " + String.valueOf(row));
-						else
-							lbl = new Label("    0");
-
-						rect.getStyleClass().add("borders");
-						lbl.getStyleClass().add("textStyleBorder");
-					} else {
-						lbl = new Label("   " + row + " * " + col);
-
-						rect.getStyleClass().add("otherBoxes");
-						lbl.getStyleClass().add("textStyleOther");
-						
-						// Start of Event Handler
-						butt.setOnAction(new EventHandler<ActionEvent>() {
-
-							@Override
-							public void handle(ActionEvent e) {
-								List<Integer> numbs = new ArrayList<Integer>();
-								List<Integer> listOfCol = new ArrayList<Integer>();
-								List<Integer> listOfRow = new ArrayList<Integer>();
-
-								rect.getStyleClass().add("otherBoxes");
-								lbl.getStyleClass().add("textStyleOther");
-
-								int userNum = Integer.parseInt(userInput.getText());
-
-								for (int i = 1; i < 11; i++) {
-									if (userNum % i == 0) {
-										numbs.add(i);
-									}
-								}
-								for (int i = 0; i < numbs.size(); i++)
-									for (int j = 0; j < numbs.size(); j++) {
-										if (numbs.get(i) * numbs.get(j) == userNum) {
-											// col = numbs.get(i);
-											// row = numbs.get(j);
-											// rect.getStyleClass().add("answerLightUp");
-											// gp.add(rect, col, row);
-											// lbl.getStyleClass().add("answerText");
-											// gp.add(lbl, col, row);
-											listOfCol.add(numbs.get(i));
-											listOfRow.add(numbs.get(j));
-										}
-									}
-								int count = 0;
-								do {
-									for (int i = 0; i < listOfCol.size(); i++)
-										for (int j = 0; j < listOfRow.size(); j++) {
-
-										}
-
-									count++;
-								} while (count < listOfCol.size());
-							}
-
-						});
-						// End of Event Handler
-					}
-
-					// rect.getStyleClass().add("rectDimensions");
-					// Style class does not let me make the dimensions for some
-					// reason
-					rect.setWidth(75);
-					rect.setHeight(75);
-					gp.add(rect, col, row);
-					gp.add(lbl, col, row);
-
-				}
-			//adding elements the the horizontal box
+			// adding elements to the horizontal box
 			hb.getChildren().addAll(askAnswerLabel, userInput, butt);
-			//combining the horizontal box & the grid pane into a vertical box
-			vb.getChildren().addAll(hb, gp);
 			
+			// combining the horizontal box & the grid pane into a vertical box
+			vb.getChildren().addAll(hb, gp);
+
 			bp.setTop(title);
 			bp.setBottom(vb);
 
@@ -148,6 +60,65 @@ public class Main extends Application {
 
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	public void setLabels() {
+		lbl = new Label[11][11];
+		
+		//creating labels and setting the rows and col to the label
+		for (int row = 0; row < 11; row++) {
+			for (int col = 0; col < 11; col++) {
+				Label l = new Label();
+				setGrid(l, row, col);
+				lbl[row][col] = l;
+				gp.add(l, col, row);
+			}
+		}
+	}
+
+	public void setGrid(final Label l, final int row, final int col) {
+
+		// populating the grid and creating the labels and setting the specific
+		// style class to where i want them
+		if (row == 0 || col == 0) {
+			l.getStyleClass().add("labelBorder");
+			if (col == 0) l.setText(String.valueOf(row));
+			else if (row == 0) l.setText(String.valueOf(col));
+			else l.setText("0");
+		} else {
+			l.setText(String.valueOf(row) + " * " + String.valueOf(col));
+			l.getStyleClass().add("labelOther");
+		}
+		
+		try{
+		// Start of Event Handler
+		butt.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e) {
+				int userNum = Integer.parseInt(userInput.getText());
+				
+				//Called the method to reset the grid back to normal
+				setLabels();
+				
+				//iterates through the numbers from 1 - 10 and when it equals to the 
+				//number entered by the user it edits the style for that label
+				for (int i = 0; i < 11; i++) {
+					for (int j = 0; j < 11; j++) {
+						if (i * j == userNum) {
+							gp.getChildren().remove(lbl[i][j]);
+							gp.getChildren().add(lbl[i][j]);
+							lbl[i][j].getStyleClass().add("labelAnswer");
+						}
+					}
+				}
+			}
+
+		});
+		// End of event handler
+		}catch(Exception e){
+			JOptionPane.showMessageDialog(null, "No Input Detected!");
 		}
 	}
 
